facb6ef532e43184434ab0b56bdb5fa5e9237900
A1Urrea <104873584+A1Urrea@users.noreply.github.com>
2022-05-12 01:16:11 -0700

    Update scripts.js
    rearrange some code and removed some i didnt need

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-<script src="js/scripts.js">
 console.log("Hi! Welcome to My Portfolio Site")
-</script>
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
